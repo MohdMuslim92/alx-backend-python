177d5a8263b1561ae4b2ed6940c9faa287c03ab8
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-11 21:33:49 +0300

    Update 4-tasks.py
    - Utilize asyncio.as_completed() for better task processing.

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -20,4 +20,4 @@ async def task_wait_n(n: int, max_delay: int) -> list:
         List[float]: List of delays in ascending order.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    return [await task for task in asyncio.as_completed(tasks)]
