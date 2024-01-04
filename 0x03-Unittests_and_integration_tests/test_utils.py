015ed2db678aaf882395fa103e0181fd786a2c75
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-05 00:23:13 +0300

    Test access_nested_map function for exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,16 @@ class TestAccessNestedMap(unittest.TestCase):
             path: Sequence, expected_result: Any) -> None:
         """Test access_nested_map function with various inputs"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map: Mapping,
+            path: Sequence, key_error_key: str) -> None:
+        """Test access_nested_map function for exceptions"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception).strip("'"), key_error_key)
