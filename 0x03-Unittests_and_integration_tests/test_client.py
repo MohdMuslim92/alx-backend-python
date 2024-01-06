f42405114f74f7aa8bd425a43713591ebc9df9f4
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-06 16:26:33 +0300

    Test the _public_repos_url method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ from client import GithubOrgClient
 from parameterized import parameterized
 from typing import Dict
 import unittest
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock, Mock, patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -24,3 +24,17 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked.assert_called_once_with(
                 "https://api.github.com/orgs/{}".format(org)
                 )
+
+    def test_public_repos_url(self):
+        """Tests the _public_repos_url method."""
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_orgs:
+            mock_orgs.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
